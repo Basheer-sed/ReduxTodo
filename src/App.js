412f6a17a6566113f84aae2a0ce5da36ef1a8dd7
Basheer-sed <basheer.sed@gmail.com>
2023-07-14 08:32:14 +0530

    version 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import {  AppBar, Box, CssBaseline, Toolbar, Typography} from '@mui/material';
 import './App.css';
 import AddTodo from './components/AddTodo';
 
 
+
 function App() {
   return (
-    <div className="App">
+    <>
+    <Box marginBottom={2} >
+       <CssBaseline>
+         <AppBar position='relative' sx={{ '@media (max-width: 600px)': { height: '60px' } }}>
+           <Toolbar sx={{justifyContent:"center"}}>
+             <Typography version="h4" align="center" fontSize={30}>
+               Todos App
+             </Typography>
+           </Toolbar>
+         </AppBar>
+       </CssBaseline>
+    </Box>
+    <Box display="flex" flexDirection="column" alignItems="center">
       <AddTodo/>
-    </div>
+    </Box>
+    </>
   );
 }
 
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import { useState } from 'react'
-import Todo from './Todo'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTodo, updateTodo} from './todoSlice'
+import React from 'react';
+import { useState } from 'react';
+import Todo from './Todo';
+import { useDispatch } from 'react-redux';
+import { addTodo, updateTodo } from './todoSlice';
+import { Box, Button, Stack, TextField } from '@mui/material';
+
+
 
 const AddTodo = () => {
-  const [todoState, setTodoState] = useState({text:'',editStatus: false, markTodo: false})
-  const dispatch = useDispatch()
+  const [todoState, setTodoState] = useState({ text: '', editStatus: false, markTodo: false });
+  const dispatch = useDispatch();
 
-  const handleChange = (e) => setTodoState({...todoState,text:e.target.value})
+  const handleChange = (e) => setTodoState({ ...todoState, text: e.target.value });
 
   const handleClick = () => {
-    if(todoState.text !==''){
-    dispatch(addTodo(todoState))
+    if (todoState.text !== '') {
+      dispatch(addTodo(todoState));
     }
-    setTodoState({...todoState,text:''})
-  }
+    setTodoState({ ...todoState, text: '' });
+  };
 
   const checkEnter = (event) => {
     if ((event.keyCode === 13 || event.which === 13) && todoState.editStatus) {
@@ -24,28 +27,29 @@ const AddTodo = () => {
       handleClick();
     }
   };
-  
 
   const handleUpdate = () => {
-    if(todoState.text!==''){
-      dispatch((updateTodo(todoState)))
+    if (todoState.text !== '') {
+      dispatch(updateTodo(todoState));
     }
-    setTodoState({...todoState,text:'', editStatus: false})
-  }
+    setTodoState({ ...todoState, text: '', editStatus: false });
+  };
 
   return (
     <>
-      <div>
-        <input type='text' value={todoState.text} onChange={handleChange} onKeyDown={checkEnter}/>
-        {todoState.editStatus ? 
-          <button onClick={handleUpdate}>Update Todo</button>
-         : 
-          <button onClick={handleClick}>Add Todo</button>
-         } 
-      </div>
-      <Todo todoState={todoState} setTodoState={setTodoState}/>
+      <Stack direction="row" spacing={2} marginTop={3}>  
+        <TextField id="outlined-basic" variant="outlined" value={todoState.text} onChange={handleChange} onKeyDown={checkEnter} />
+        {todoState.editStatus ? (
+          <Button variant="contained" onClick={handleUpdate}>Update Todo</Button>
+        ) : (
+          <Button variant="contained" onClick={handleClick}>Add Todo</Button>
+        )}
+      </Stack>
+      <Box display="flex" flexDirection="column" alignItems="center">
+      <Todo todoState={todoState} setTodoState={setTodoState} />
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default AddTodo
+export default AddTodo;
